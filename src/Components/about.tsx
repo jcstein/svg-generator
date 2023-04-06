ea7cd36cf94c69ef78b8e7bb56b099da78c850b8
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-04-06 19:11:58 -0400

    copy edit

--- src/Components/about.tsx
+++ src/Components/about.tsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <VStack pb="5">
       <Heading pb="3" textAlign={"center"}>🟣 Base64 SVG Tutorial 🟣</Heading>
-      <Text>This tutorial will guide you through the process of generating a custom SVG, posting it to Celestia's data availability layer, retrieving it, and displaying it back.</Text>
+      <Text>This tutorial will guide you through the process of generating a custom SVG, posting it to Celestia's data availability layer, retrieving it, and validating the retrieval by displaying it back.</Text>
       <Text>The only pre-requisite for this tutorial is to <Link href="https://docs.celestia.org/developers/node-tutorial" isExternal>run a Celestia light node</Link> that is funded with testnet tokens. It is also recommended to read about the <Link href="https://docs.celestia.org/developers/rpc-tutorial/" isExternal>RPC API and the Celestia Node CLI guide.</Link></Text>
     </VStack>
   );
