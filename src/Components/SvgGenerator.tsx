5b8d1ca7cd086f50d7898cbb73c7d4d2020fe2d0
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-10-10 18:03:55 -0400

    Update SvgGenerator.tsx

--- src/Components/SvgGenerator.tsx
+++ src/Components/SvgGenerator.tsx
@@ -94,59 +94,39 @@ const SvgGenerator: React.FC<Props> = () => {
               <Heading size="md" pb="3">4. Post the data to Celestia as plain text 🚀</Heading>
               <Text>First, set your auth token:</Text>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export CELESTIA_NODE_AUTH_TOKEN=$(celestia light auth admin --p2p.network arabica)</Code>
-                <ClipboardButton text='export CELESTIA_NODE_AUTH_TOKEN=$(celestia light auth admin --p2p.network arabica)' />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export AUTH_TOKEN=$(celestia light auth admin --p2p.network mocha)</Code>
+                <ClipboardButton text='export AUTH_TOKEN=$(celestia light auth admin --p2p.network mocha)' />
               </Flex>
               <Text>Next, post the SVG and save the output:</Text>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export OUTPUT=$(celestia rpc blob Submit 0x42690c204d39600fddd3 "\"$SVG\"")</Code>
-                <ClipboardButton text={`export OUTPUT=$(celestia rpc blob Submit 0x42690c204d39600fddd3 "\\"$SVG\\"")`} />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export OUTPUT=$(celestia blob submit 0x42690c204d39600fddd3 "$SVG" --token $AUTH_TOKEN)</Code>
+                <ClipboardButton text={`export OUTPUT=$(celestia blob submit 0x42690c204d39600fddd3 "$SVG" --token $AUTH_TOKEN)`} />
               </Flex>
               <br />
               <Heading size="md" pb="3">5. Set the block height to retrieve your data 🧊</Heading>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export HEIGHT=$(echo "$OUTPUT" | jq '.result') && echo "Height: $HEIGHT"</Code>
-                <ClipboardButton text={`export HEIGHT=$(echo "$OUTPUT" | jq '.result') && echo "Height: $HEIGHT"`} />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">export HEIGHT=$(echo "$OUTPUT" | jq '.result.height') && echo "Height: $HEIGHT"</Code>
+                <ClipboardButton text={`export HEIGHT=$(echo "$OUTPUT" | jq '.result.height') && echo "Height: $HEIGHT"`} />
               </Flex>
               <br />
               <Heading size="md" pb="3">6. Retrieve the data from Celestia ✨</Heading>
               <Text>Retrieve the shares by namespace and block height:</Text>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3</Code>
-                <ClipboardButton text={`celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3`} />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN</Code>
+                <ClipboardButton text={`celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN`} />
               </Flex>
               <Text>Display only the data retrieved:</Text>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3 | jq '.result[0].data'</Code>
-                <ClipboardButton text={`celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3 | jq '.result[0].data'`} />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN | jq '.result[0].data'</Code>
+                <ClipboardButton text={`celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN | jq '.result[0].data'`} />
               </Flex>
               <Text>Copy only the data retrieved, without quotes, to your clipboard:</Text>
               <Flex>
-                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3 | jq '.result[0].data' | pbcopy</Code>
-                <ClipboardButton text={`celestia rpc blob GetAll $HEIGHT 0x42690c204d39600fddd3 | jq '.result[0].data' | pbcopy`} />
+                <Code p={2} fontSize="sm" borderRadius="md" whiteSpace="pre-wrap">celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN | jq -r '.result[0].data' | pbcopy</Code>
+                <ClipboardButton text={`celestia blob get-all $HEIGHT 0x42690c204d39600fddd3 --token $AUTH_TOKEN | jq -r '.result[0].data' | pbcopy`} />
               </Flex>
               <br/>
-              <Heading size="md" pb="3">7. Convert to text and parse out the metadata</Heading>
-              <Textarea
-                placeholder="Paste base64 input from step 6"
-                onChange={handleDecodeBase64}
-                textAlign="left"
-                maxWidth="1000px"
-                minWidth="300px"
-                resize="vertical"
-              />
-              <Text>Copy <Code>data:image/svg+xml;base64,abcdefg[...]A=</Code> to paste in step 8</Text>
-              <Textarea
-                mt={2}
-                value={decodedText}
-                isReadOnly
-                maxWidth="1000px"
-                minWidth="300px"
-                minHeight="125px"
-                placeholder="Decoded data retreived from Celestia"
-              />
-              <br />
-              <Heading size="md" pb="3">8. Display the SVG you retrieved from Celestia</Heading>
+              <Heading size="md" pb="3">7. Display the SVG you retrieved from Celestia</Heading>
               <Textarea
                 placeholder="Enter the parsed base64 SVG"
                 value={displaySvg}
@@ -162,7 +142,7 @@ const SvgGenerator: React.FC<Props> = () => {
                   <Text mb="3">Do you want to see that it really worked? Paste the SVG into your browser's search bar 🧙‍♂️</Text>
                   <Text>Did you have fun with this tutorial? Share a screenshot of your SVG on your favorite social media platform ✍️</Text>
                   <br/>
-                  <Heading size="md" pb="3">9. Build your next project at <Link href="https://docs.celestia.org" isExternal>docs.celestia.org</Link></Heading>
+                  <Heading size="md" pb="3">8. Build your next project at <Link href="https://docs.celestia.org" isExternal>docs.celestia.org</Link></Heading>
                 </Box>
               )}
             </VStack>
